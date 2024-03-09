598a8e3abc085d3d938ca78493944b2ccc22e885
sisindrik <sisindrik@lyrostech.com>
2024-03-09 15:23:30 +0530

    font change

---
+++ src/App15.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaHome } from "react-icons/fa";
+import { FaBookmark } from "react-icons/fa";
+import { FaPhotoVideo } from "react-icons/fa";
+
+
+
+
+export const App15 = () => {
+  return (
+    <div>
+        <p><FaHome/>Home</p>
+        <p><FaBookmark />Post</p>
+        <p> <FaPhotoVideo />Photos</p>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App6 from "./App7";
-// import App7 from "./App7";
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <>
-//     <App7 />
-//   </>
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App6 from "./App7";
+import App7 from "./App7";
+import { App15 } from "./App15";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App15/>
+  </>
+);
 
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App14 from './App14';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App15 from './App15';
 
-const root = document.getElementById("root");
-ReactDOM.render(<App14 />, root);
+
+// const root = document.getElementById("root");
+// ReactDOM.render(<App15 />, root);
